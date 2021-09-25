42dff63bc528eb49a643ee9f1e6c4a54af418cd5
JuanC-JC <juancchaparro97@gmail.com>
2021-09-25 01:46:31 -0500

    fixed bug render interactive map - home

--- src/components/Home/NewSection.jsx
+++ src/components/Home/NewSection.jsx
@@ -5,35 +5,35 @@ import '../../styles/components/Home/NewSection.scss';
 import photoNew from '../../images/bosque.jpg'
 
 const news = [
-  { 
-    id:1,
-    date:'09 Ago 2021',
-    notification:true,
-    title:'Consectetur adipiscing elit.',
+  {
+    id: 1,
+    date: '09 Ago 2021',
+    notification: true,
+    title: 'Consectetur adipiscing elit.',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.',
     img: photoNew
   },
-  { 
-    id:2,
-    date:'09 Ago 2021',
-    notification:false,
-    title:'Consectetur adipiscing elit.',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.',    
-    img: photoNew
-  },
-  { 
-    id:3,
-    date:'09 Ago 2021',
-    notification:true,
-    title:'Consectetur adipiscing elit.',
+  {
+    id: 2,
+    date: '09 Ago 2021',
+    notification: false,
+    title: 'Consectetur adipiscing elit.',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.',
     img: photoNew
   },
-  { 
-    id:4,
-    date:'09 Ago 2021',
-    notification:false,
-    title:'Consectetur adipiscing elit.',
+  {
+    id: 3,
+    date: '09 Ago 2021',
+    notification: true,
+    title: 'Consectetur adipiscing elit.',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.',
+    img: photoNew
+  },
+  {
+    id: 4,
+    date: '09 Ago 2021',
+    notification: false,
+    title: 'Consectetur adipiscing elit.',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate egestas bibendum in odio.',
     img: photoNew
   },
@@ -41,14 +41,14 @@ const news = [
 
 
 
-export default function NewSection (){
-  return(
-    <section class='c-new'>
+export default function NewSection() {
+  return (
+    <section className='c-new'>
 
       <h2>Noticias</h2>
       <div className='news'>
         {
-          news.map( report =>
+          news.map(report =>
             <New
               key={report.id}
               id={report.id}
