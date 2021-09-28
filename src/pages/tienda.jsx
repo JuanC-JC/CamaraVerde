1ed246b1e3ec0b430b7733f5373d4d6f946a8617
isabelajs <isabelajimenezs96@gmail.com>
2021-09-27 20:41:23 -0500

    di espacios en documents y cree el componente store

---
+++ src/pages/tienda.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+//componentes
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Store from '../components/Store/Store'
+
+//estilos e imagenes
+import '../styles/global.scss';
+
+
+
+export default function StorePage (){
+  return(
+    <>
+      <Header/>
+      <Store/>
+      <Footer/>
+    </>
+  )
+}
