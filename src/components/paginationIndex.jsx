fdc62e98a6f8e2dc127718f5a27ae506b3f3b171
isabelajs <isabelajimenezs96@gmail.com>
2021-10-04 17:03:39 -0500

    modify paginationIndes´s Imag

--- src/components/paginationIndex.jsx
+++ src/components/paginationIndex.jsx
@@ -5,23 +5,16 @@ import '../styles/paginationIndex.scss'
 export default function PaginationIndex () {
   return(
     <div className="paginationIndex">
-      <Link to='' className='previous'>
-      <svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M20 2L15 7L20 12L19 14L12 7L19 0L20 2Z" fill="black"/>
-        <path d="M8 2L3 7L8 12L7 14L0 7L7 0L8 2Z" fill="black"/>
-      </svg>
-
+      <Link to=''>
+        <div className='icon previous'></div>
       </Link>
 
       <div className='indexNumber'>1</div>
       <div className='indexNumber'>2</div>
       <div className='indexNumber'>3</div>
 
-      <Link to='' className='next'>
-        <svg width="20" height="14" viewBox="0 0 20 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 2L5 7L0 12L1 14L8 7L1 0L0 2Z" fill="black"/>
-          <path d="M12 2L17 7L12 12L13 14L20 7L13 0L12 2Z" fill="black"/>
-        </svg>
+      <Link to=''>
+        <div className='icon next'></div>
       </Link>
     </div>
   )
